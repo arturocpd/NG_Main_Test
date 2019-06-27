e8484e2722715badbf0d2f047f08b66cc15a3d98
Arturocpd <ar@copa.do>
2019-06-27 13:27:03 +0200

    delete vlocity folder

--- vlocity/OmniScript/VDO_Business-Order-Mobile_English/VDO_Business-Order-Mobile_English_JavaScript.js
+++
@@ -1,6 +0,0 @@
-function clicked(){
-document.getElementById('Region').value = 'EU';
-}
-
-document.getElementById('euclick').onclick = clicked;
-document.getElementById('naclick').onclick = clicked;
